4bb13a3e1ad4f1e6f5f8606c508ea27df3d3ba80
Dávid Czuri <czuridavid@hotmail.com>
2019-04-29 17:04:18 +0200

    psw chk MW

--- middleware/auth/checkPassMW.js
+++ middleware/auth/checkPassMW.js
@@ -6,6 +6,18 @@ const requireOption = require('../requireOption');
 
 module.exports = function (objectrepository) {
     return function (req, res, next) {
-        next();
+        if(typeof req.body === 'undefined' || typeof req.body.password === 'undefined'){
+            return next();
+        }
+        //pw check
+        if(req.body.password !== 'asd'){
+            return next();
+        }
+        //session
+        req.session.sessionid = 1;
+
+        return res.redirect('/');
+
+
     };
 };
