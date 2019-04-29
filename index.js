ade7e6a01007426f6345d351092c2ddd91ae52a4
Dávid Czuri <czuridavid@hotmail.com>
2019-04-29 17:03:53 +0200

    added sessions and json parser

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 let express = require('express');
 let app = express();
 
+let session = require('express-session');
+let bodyParser = require('body-parser');
+
 //app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
+//sessions
+app.use(session({
+	secret: 'supersecret',
+	cookie: {
+		maxAge: 99999
+	},
+	resave: true,
+	saveUninitialized: false
+}));
+
+//post parsing to json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
+
 require('./route/index')(app);
+
+//basic error handler
+app.use(function(err, req, res, next){
+	res.status(500).send('Problem');
+
+	console.error(err.stack);
+})
 
 let server = app.listen(3000, function(){
 	console.log('listening on port:3000');
